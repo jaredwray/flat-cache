f7ea547c43495166a70fa444a31ac55300b6ceb2
Jimmy Wärting <jimmy@warting.se>
2023-08-15 18:13:16 +0200

    make cache a class

--- src/cache.js
+++ src/cache.js
@@ -4,17 +4,17 @@ var utils = require('./utils');
 var del = require('./del');
 var writeJSON = utils.writeJSON;
 
-var cache = {
+class Cache {
   /**
    * Load a cache identified by the given Id. If the element does not exists, then initialize an empty
    * cache storage. If specified `cacheDir` will be used as the directory to persist the data to. If omitted
    * then the cache module directory `./cache` will be used instead
    *
    * @method load
-   * @param docId {String} the id of the cache, would also be used as the name of the file cache
-   * @param [cacheDir] {String} directory for the cache entry
+   * @param {String} docId the id of the cache, would also be used as the name of the file cache
+   * @param {String} [cacheDir] directory for the cache entry
    */
-  load: function (docId, cacheDir) {
+  load(docId, cacheDir) {
     var me = this;
 
     me._visited = {};
@@ -24,62 +24,65 @@ var cache = {
     if (fs.existsSync(me._pathToFile)) {
       me._persisted = utils.tryParse(me._pathToFile, {});
     }
-  },
+  }
 
   /**
    * Load the cache from the provided file
    * @method loadFile
    * @param  {String} pathToFile the path to the file containing the info for the cache
    */
-  loadFile: function (pathToFile) {
+  loadFile(pathToFile) {
     var me = this;
     var dir = path.dirname(pathToFile);
     var fName = path.basename(pathToFile);
 
     me.load(fName, dir);
-  },
+  }
 
   /**
    * Returns the entire persisted object
    * @method all
    * @returns {*}
    */
-  all: function () {
+  all() {
     return this._persisted;
-  },
+  }
 
-  keys: function () {
+  keys() {
     return Object.keys(this._persisted);
-  },
+  }
+
   /**
    * sets a key to a given value
    * @method setKey
    * @param key {string} the key to set
    * @param value {object} the value of the key. Could be any object that can be serialized with JSON.stringify
    */
-  setKey: function (key, value) {
+  setKey(key, value) {
     this._visited[key] = true;
     this._persisted[key] = value;
-  },
+  }
+
   /**
    * remove a given key from the cache
    * @method removeKey
    * @param key {String} the key to remove from the object
    */
-  removeKey: function (key) {
+  removeKey(key) {
     delete this._visited[key]; // esfmt-ignore-line
     delete this._persisted[key]; // esfmt-ignore-line
-  },
+  }
+
   /**
    * Return the value of the provided key
    * @method getKey
    * @param key {String} the name of the key to retrieve
    * @returns {*} the value from the key
    */
-  getKey: function (key) {
+  getKey(key) {
     this._visited[key] = true;
     return this._persisted[key];
-  },
+  }
 
   /**
    * Remove keys that were not accessed/set since the
@@ -87,7 +90,7 @@ var cache = {
    * @method _prune
    * @private
    */
-  _prune: function () {
+  _prune() {
     var me = this;
     var obj = {};
 
@@ -104,7 +107,7 @@ var cache = {
 
     me._visited = {};
     me._persisted = obj;
-  },
+  }
 
   /**
    * Save the state of the cache identified by the docId to disk
@@ -112,33 +115,34 @@ var cache = {
    * @param [noPrune=false] {Boolean} whether to remove from cache the non visited files
    * @method save
    */
-  save: function (noPrune) {
+  save(noPrune) {
     var me = this;
 
     !noPrune && me._prune();
     writeJSON(me._pathToFile, me._persisted);
-  },
+  }
 
   /**
    * remove the file where the cache is persisted
    * @method removeCacheFile
    * @return {Boolean} true or false if the file was successfully deleted
    */
-  removeCacheFile: function () {
+  removeCacheFile() {
     return del(this._pathToFile);
-  },
+  }
+
   /**
    * Destroy the file cache and cache content.
    * @method destroy
    */
-  destroy: function () {
+  destroy() {
     var me = this;
     me._visited = {};
     me._persisted = {};
 
     me.removeCacheFile();
-  },
-};
+  }
+}
 
 module.exports = {
   /**
@@ -147,7 +151,7 @@ module.exports = {
    * @method load
    * @param docId {String} the id of the cache, would also be used as the name of the file cache
    * @param [cacheDir] {String} directory for the cache entry
-   * @returns {cache} cache instance
+   * @returns {Cache} cache instance
    */
   load: function (docId, cacheDir) {
     return this.create(docId, cacheDir);
@@ -160,16 +164,16 @@ module.exports = {
    * @method create
    * @param docId {String} the id of the cache, would also be used as the name of the file cache
    * @param [cacheDir] {String} directory for the cache entry
-   * @returns {cache} cache instance
+   * @returns {Cache} cache instance
    */
   create: function (docId, cacheDir) {
-    var obj = Object.create(cache);
+    var obj = new Cache();
     obj.load(docId, cacheDir);
     return obj;
   },
 
   createFromFile: function (filePath) {
-    var obj = Object.create(cache);
+    var obj = new Cache();
     obj.loadFile(filePath);
     return obj;
   },
